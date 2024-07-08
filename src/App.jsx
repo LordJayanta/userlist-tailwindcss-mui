19c8c4383da65b2c148631bf1981c8458c29607c
Jayanta Sarkar <sarkarjayanta327@gmail.com>
2024-07-08 08:38:21 +0530

    Update Ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import MuiPage from "./screens/MuiPage"
 import TailwindPage from "./screens/TailwindPage"
 import { NavLink } from "react-router-dom"
-
+import './App.css'
 
 function App() {
 
@@ -11,12 +11,12 @@ function App() {
         <div className="w-[60%] h-[60%] bg-[#262626] flex justify-around items-center md:flex-row flex-col rounded-xl shadow-2xl">
           <NavLink to={'/mui'} className="md:w-[25%] w-[50%]">
             <figure>
-              <img className="w-full" src="./svg/icons8-material-ui.svg" alt="" />
+              <img className="logo logo-material-ui" src="./svg/icons8-material-ui.svg" alt="" />
             </figure>
           </NavLink>
           <NavLink to={'/tailwindcss'} className="md:w-[25%] w-[50%]">
             <figure>
-              <img className="w-full" src="./svg/icons8-tailwindcss.svg" alt="" />
+              <img className="logo logo-tailwindcss" src="./svg/icons8-tailwindcss.svg" alt="" />
             </figure>
           </NavLink>
         </div>
